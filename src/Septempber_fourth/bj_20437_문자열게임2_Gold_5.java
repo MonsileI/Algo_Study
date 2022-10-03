e0db91a9443d90d2e3abb691e8489db8e10e41ff
monsileI <hansaem900d@kakao.com>
2022-10-03 18:03:14 +0900

    1003 알고리즘 제출

---
+++ src/Septempber_fourth/bj_20437_문자열게임2_Gold_5.java
@@ -0,0 +1,25 @@
+package Septempber_fourth;
+
+import java.io.*;
+import java.util.*;
+
+public class bj_20437_문자열게임2_Gold_5 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		
+		int tc = Integer.parseInt(br.readLine());
+		
+		for(int d=0;d<tc;d++) {
+			
+			String str = br.readLine();
+			int n = Integer.parseInt(br.readLine());
+			
+			
+			
+			
+		}
+		
+	}
+}
