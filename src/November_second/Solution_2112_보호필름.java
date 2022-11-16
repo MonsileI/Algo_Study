25a51e496d09f5ffab0c4d48c280c18f9186215d
hansaem900d <hansaem900d@kakao.com>
2022-11-16 18:31:51 +0900

    보호필름 커밋

--- src/November_second/Solution_2112_보호필름.java
+++ src/November_second/Solution_2112_보호필름.java
@@ -7,10 +7,74 @@ public class Solution_2112_보호필름 {
 
 	static int N,M,K,map[][];
 	static int answer;
-	static boolean[]visited;
+	static int[]visited;
+	
+	static void calc(int sum) {
+		
+		
+		boolean flag = false;
+
+		for(int j=0;j<M;j++) {
+			
+			flag = false;
+			
+			int checkNum = 0;
+			
+			if(visited[0]==0) checkNum = map[0][j];
+			else if(visited[0]==1) checkNum = 1;
+			else checkNum = 0;
+			
+			int checkCnt = 1;
+			
+			
+			for(int i=1;i<N;i++) {
+				int tmpNum = 0;
+				
+				if(visited[i]==0) tmpNum = map[i][j];
+				else if(visited[i]==1) tmpNum = 1;
+				else tmpNum = 0;
+
+				if(checkNum != tmpNum) {
+					checkCnt = 1;
+					checkNum = tmpNum;
+				}else checkCnt++;
+				
+				if(checkCnt==K) {
+					flag= true;
+					break;
+				}
+				
+			}
+			
+			if(!flag) return;
+			
+		}
+	
+			answer = Math.min(answer, sum);
+			return;
+		
+		
+	}
 	
 	
-	static void sub() {
+	static void sub(int depth,int sum) {
+		
+		
+		if(answer<=sum) return;
+		
+		if(depth==N) {
+			calc(sum);
+			return;
+		}
+		
+		
+		visited[depth] = 0;
+		sub(depth+1,sum);
+		visited[depth] = 1;
+		sub(depth+1,sum+1);
+		visited[depth] = -1;
+		sub(depth+1,sum+1);
+		
 		
 	}
 	
@@ -27,8 +91,8 @@ public class Solution_2112_보호필름 {
 			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			map = new int[N][M];
-			visited = new boolean[N];
-			
+			visited = new int[N];
+			answer = 987654321;
 			for(int i=0;i<N;i++) {
 				st = new StringTokenizer(br.readLine()," ");
 				for(int j=0;j<M;j++) {
@@ -36,12 +100,10 @@ public class Solution_2112_보호필름 {
 				}
 			}
 			
+			sub(0,0);
 			
-			sub();
-			
-			
-			
+			sb.append("#").append(t).append(" ").append(answer).append("\n");
 		}
-		
+		System.out.println(sb.toString());
 	}
 }
