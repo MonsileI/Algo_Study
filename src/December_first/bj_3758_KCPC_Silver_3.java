4cb6b42b639e819c1257292e4aab923546e960cf
monsileI <hansaem900d@kakao.com>
2022-12-04 20:47:58 +0900

    kcpc 진행중

---
+++ src/December_first/bj_3758_KCPC_Silver_3.java
@@ -0,0 +1,37 @@
+package December_first;
+
+import java.util.*;
+import java.io.*;
+
+public class bj_3758_KCPC_Silver_3 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int Test_Case = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+		int n,k,t,m;
+		for(int tc=0;tc<Test_Case;tc++) {
+		
+			st = new StringTokenizer(br.readLine()," ");
+			
+			n = Integer.parseInt(st.nextToken());
+			k = Integer.parseInt(st.nextToken());
+			t = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			
+			for(int d=0;d<m;d++) {
+				
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+	}
+
+}
