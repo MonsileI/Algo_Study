f0957a41f04a00ba3e4deaa774fddfaac46ad61e
monsileI <hansaem900d@kakao.com>
2023-01-09 20:25:58 +0900

    intellij로 연동하기

---
+++ src/January_Second/test.java
@@ -0,0 +1,11 @@
+package January_Second;
+
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("intellij , github test");
+        System.out.println("intellij , github test");
+
+    }
+}
