e23499f7cdb149e5ed3b4caa632c3031afcdeb7e
monsileI <hansaem900d@kakao.com>
2023-02-01 20:12:43 +0900

    실버 5 문제

---
+++ src/January_Fifth/bj_1476_날짜계산_Silver_5.java
@@ -0,0 +1,41 @@
+package January_Fifth;
+
+import java.util.*;
+import java.io.*;
+public class bj_1476_날짜계산_Silver_5 {
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int answer =1;
+
+        int E = Integer.parseInt(st.nextToken()); //15
+        int S = Integer.parseInt(st.nextToken()); //28
+        int M = Integer.parseInt(st.nextToken()); //19
+
+        int e=1;int s=1;int m=1;
+
+        while(true){
+
+            if(e==16) e=1;
+            if(s==29) s=1;
+            if(m==20) m=1;
+
+            if(E==e && S==s && M==m) break;
+
+
+            e++;
+            s++;
+            m++;
+            answer++;
+
+        }
+
+        System.out.println(answer);
+
+
+
+    }
+}
