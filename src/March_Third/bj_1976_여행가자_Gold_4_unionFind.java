e18a0b3d1dc30c28cd8d63a63258c0c5bd7ff7b4
MonsileI <hansaem900d@naver.com>
2023-03-22 09:20:11 +0900

    유니온 파인드

--- src/March_Third/bj_1976_여행가자_Gold_4_unionFind.java
+++ src/March_Third/bj_1976_여행가자_Gold_4_unionFind.java
@@ -4,40 +4,55 @@ import java.util.*;
 import java.io.*;
 public class bj_1976_여행가자_Gold_4_unionFind {
 
-    static int[]parents;
-    static int N,M;
+    static int N,M,parents[];
     static String answer = "YES";
 
-    static void make() {
-        for(int i=1;i<N+1;i++) {
-            parents[i] = i;
-        }
+    static void make(){
+        for(int i=1;i<N+1;i++) parents[i] = i;
     }
 
-    static int find(int a) {
-
-        if(parents[a]==a) return a;
-
-        return parents[a] = find(parents[a]);
-
+    static int find(int num){
+        if(parents[num] == num) return num;
+        return parents[num] =find(parents[num]);
     }
 
-    static void union(int a,int b) {
+    static void union(int a,int b){
+        int rootA = find(a);
+        int rootB = find(b);
 
-        int aRoot = find(a);
-        int bRoot = find(b);
-
-        if(aRoot!=bRoot) {
-            if(aRoot < bRoot) {
-                parents[bRoot] = aRoot;
-            }else {
-                parents[aRoot] = bRoot;
-            }
+        if(rootA!=rootB){
+            if(rootA<rootB) parents[rootA]= rootB;
+            else parents[rootB] = rootA;
         }
+
+
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringTokenizer st = null;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        parents = new int[N+1];
+        make();
+        for(int i=1;i<N+1;i++){
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j=1;j<N+1;j++){
+                int num = Integer.parseInt(st.nextToken());
+                if(num==1){
+                    union(i,j);
+                }
+            }
+        }
+        st = new StringTokenizer(br.readLine()," ");
+        int start = find(Integer.parseInt(st.nextToken()));
+        for(int i=1;i<M;i++){
+            int num = Integer.parseInt(st.nextToken());
+            if(start != find(num)){
+                answer = "NO";
+                break;
+            }
+        }
+        System.out.println(answer);
     }
 }
