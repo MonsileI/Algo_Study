706f6f2bafc8b3405754d974bdda9ef299f52573
hansaem900d <hansaem900d@kakao.com>
2023-06-07 09:54:49 +0900

    문자열 수정

--- src/Jun_First/bj/bj_1972_놀라운문자열_Silver_3.java
+++ src/Jun_First/bj/bj_1972_놀라운문자열_Silver_3.java
@@ -12,20 +12,20 @@ public class bj_1972_놀라운문자열_Silver_3 {
             String str = br.readLine();
             if(str.equals("*")) break;
             int size = str.length();
-            for(int i=0;i<size-1;i++){
-                char firCh = str.charAt(i);
+            int idx = 1;
+            while(idx<size){
                 list = new ArrayList<>();
-                for(int j=i+1;j<size;j++){
-                    char secCh = str.charAt(j);
-                    String check = ""+firCh +secCh;
+                for(int i=0;i<size-idx;i++){
+                    String check = ""+str.charAt(i)+str.charAt(i+idx);
                     if(list.contains(check)) {
-                        sb.append(str+" is NOT surprising").append("\n");
+                        sb.append(str+" is NOT surprising.").append("\n");
                         continue OuterLoop;
                     }
                     list.add(check);
                 }
-            }
-            sb.append(str+" is surprising").append("\n");
+                idx++;
+           }
+            sb.append(str+" is surprising.").append("\n");
         }
         System.out.println(sb.toString());
     }
