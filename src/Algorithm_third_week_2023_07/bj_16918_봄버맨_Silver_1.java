bf4cb59c32930a63f77a30b9eba9e224ded8b3ba
hansaem900d <hansaem900d@kakao.com>
2023-07-19 22:21:26 +0900

    봄버맨 확인

--- src/Algorithm_third_week_2023_07/bj_16918_봄버맨_Silver_1.java
+++ src/Algorithm_third_week_2023_07/bj_16918_봄버맨_Silver_1.java
@@ -20,9 +20,50 @@ public class bj_16918_봄버맨_Silver_1 {
             String str = br.readLine();
             for (int j = 0; j < M; j++) {
                 map[i][j] = str.charAt(j);
+                if(map[i][j]=='O')q.offer(new int[]{i,j});
+            }
+        }
+        for(int t=2;t<T+1;t++){
+
+
+            if(t%2==0){
+                for(int i=0;i<N;i++){
+                    for(int j=0;j<M;j++){
+                        map[i][j] = 'O';
+                    }
+                }
+            }else{
+                bomb();
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++){
+                sb.append(map[i][j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+
+    static void bomb(){
+
+        while(!q.isEmpty()){
+            int[]c = q.poll();
+            int i = c[0]; int j = c[1];
+            map[i][j] = '.';
+            for(int d=0;d<4;d++){
+                int ni = i + move[d][0]; int nj = j + move[d][1];
+                if(ni<0||nj<0||N-1<ni||M-1<nj) continue;
+                map[ni][nj] = '.';
             }
         }
 
+        for(int i=0;i<N;i++){
+            for(int j=0;j<M;j++) {
+                if(map[i][j]=='O') q.offer(new int[]{i,j});
+            }
+        }
 
     }
 }
