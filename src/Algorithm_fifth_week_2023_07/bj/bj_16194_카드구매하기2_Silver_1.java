c3700062193eee385a846e9123ae84ca7b4a973e
hansaem900d <hansaem900d@kakao.com>
2023-08-09 00:35:52 +0900

    창영이와 커피 결국 풀이봄... 풀 수 있었는데! 아 진짜 억울해 이게 왜 틀려

--- src/Algorithm_fifth_week_2023_07/bj/bj_16194_카드구매하기2_Silver_1.java
+++ src/Algorithm_fifth_week_2023_07/bj/bj_16194_카드구매하기2_Silver_1.java
@@ -6,10 +6,17 @@ public class bj_16194_카드구매하기2_Silver_1 {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N= Integer.parseInt(br.readLine());
-        int[]arr = new int[N];
+        int[]arr = new int[N+1];
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        for(int i=0;i<N;i++)arr[i] = Integer.parseInt(st.nextToken());
-
-
+        for(int i=1;i<N+1;i++)arr[i] = Integer.parseInt(st.nextToken());
+        int[]dp = new int[N+1];
+        for(int i=1;i<N+1;i++){
+            dp[i] = arr[i];
+            for(int j=1;j<=i;j++){
+                dp[i] = Math.min(dp[i],dp[i-j]+arr[j]);
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        System.out.println(dp[N]);
     }
 }
--- src/Algorithm_sixth_week_2023_08/bj/bj_22115_창영이와커피_Gold_5.java
+++ src/Algorithm_sixth_week_2023_08/bj/bj_22115_창영이와커피_Gold_5.java
@@ -1,42 +1,33 @@
 package Algorithm_sixth_week_2023_08.bj;
 
-import java.util.*;
 import java.io.*;
-public class bj_22115_창영이와커피_Gold_5 {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine()," ");
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[]arr= new int[N+1];
-        int answer = 1;
-        st = new StringTokenizer(br.readLine());
-        int total = 0;
-        for(int i=1;i<N+1;i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            total += arr[i];
-        }
-        if(total<K) answer = -1;
-        else{
-            int[][]dp = new int[N+1][K+1];
-            for(int i=1;i<N+1;i++){
-                for(int j=1;j<K+1;j++){
-                    dp[i][j] = 987654321;
-                }
-            }
+import java.util.*;
 
-            for(int i=1;i<N+1;i++){
-                int num = arr[i];
-                for(int j=1;j<K+1;j++){
-                    if(dp[i-1][j]!=0) dp[i][j] = dp[i-1][j];
-                    if(arr[i]<=j){
-                        dp[i][j] = Math.min(dp[i][j],dp[i][j-arr[i]]+1);
-                    }
+public class bj_22115_창영이와커피_Gold_5 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine()," ");
+        int N = Integer.parseInt(st.nextToken());int K = Integer.parseInt(st.nextToken());
+        int[]arr = new int[N+1];
+        int[][]dp = new int[N+1][K+1];
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i=1;i<N+1;i++)arr[i] = Integer.parseInt(st.nextToken());
+        for(int i=0;i<N+1;i++){
+            for(int j=0;j<K+1;j++) {
+                dp[i][j] = 987654321;
+            }
+        }
+        for(int i=0;i<N+1;i++) dp[i][0] = 0;
+        for(int i=1;i<N+1;i++){
+            for(int j=1;j<K+1;j++){
+                if(j-arr[i]<0){
+                    dp[i][j] = dp[i-1][j];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j],dp[i-1][j-arr[i]]+1);
                 }
             }
-            answer = (dp[N][K]==987654321) ? -1 : dp[N][K];
         }
-        System.out.println(answer);   
-    
+        int answer = dp[N][K] == 987654321 ? -1 : dp[N][K];
+        System.out.println(answer);
     }
 }
