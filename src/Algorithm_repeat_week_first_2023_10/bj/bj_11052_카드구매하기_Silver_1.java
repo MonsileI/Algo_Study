64d8fc6806d958832d38c876ef2601b60b87d171
hansaem900d <hansaem900d@kakao.com>
2023-10-13 18:30:20 +0900

    카드 구매하기 dp silver 1

---
+++ src/Algorithm_repeat_week_first_2023_10/bj/bj_11052_카드구매하기_Silver_1.java
@@ -0,0 +1,22 @@
+package Algorithm_repeat_week_first_2023_10.bj;
+
+import java.util.*;
+import java.io.*;
+public class bj_11052_카드구매하기_Silver_1 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        int[]arr = new int[N+1];
+        for(int i=1;i<N+1;i++)arr[i] = Integer.parseInt(st.nextToken());
+        int[][]dp = new int[N+1][N+1];
+        for(int i=1;i<N+1;i++){
+            int v = arr[i];
+            for(int j=0;j<N+1;j++){
+                dp[i][j] = dp[i-1][j];
+                if(i<=j) dp[i][j] = Math.max(dp[i][j],dp[i][j-i]+v);
+            }
+        }
+        System.out.println(dp[N][N]);
+    }
+}
